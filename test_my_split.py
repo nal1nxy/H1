8fdbb33bc21380baeef341ff82f39214b5e971f7
NalinYet <nalin.yetukuri@unh.edu>
2024-09-24 17:37:50 -0400

    Test cases written in test_my_split.py

--- test_my_split.py
+++ test_my_split.py
@@ -1,6 +1,32 @@
 """
 File: test_my_split.py
-Initial developers: COMP 801 instructors
 Developer: Nalin Yetukuri
 Date: 9/24/2024
 """
+
+from sentence import Sentence
+
+
+def test_my_split_multi_line():
+    """
+    Test Sentence that has multiple lines.
+    """
+    test_sentence = """This is a string
+    in multiple lines"""
+    sentence_object = Sentence(test_sentence)
+    expected = ['This', 'is', 'a', 'string', 'in', 'multiple', 'lines']
+    sentence_object.my_split()
+    actual = sentence_object.words
+    assert actual == expected
+
+
+def test_my_split_chars():
+    """
+    Test sentence that has special characters with extra space in the end.
+    """
+    test_sentence = "special octets for 802.1Q?). "
+    sentence_object = Sentence(test_sentence)
+    expected = ['special', 'octets', 'for', '802.1Q?).']
+    sentence_object.my_split()
+    actual = sentence_object.words
+    assert actual == expected
