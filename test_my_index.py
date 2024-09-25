3177e8c56be1944c46ef544eb13d8b0f14547604
NalinYet <nalin.yetukuri@unh.edu>
2024-09-25 17:23:36 -0400

    Test functions for the method my_index

--- test_my_index.py
+++ test_my_index.py
@@ -4,3 +4,28 @@ Initial developers: COMP 801 instructors
 Developer: Nalin Yetukuri
 Date: 9/24/2024
 """
+from sentence import Sentence
+
+
+def test_my_index_a_word():
+    """
+    Test Sentence that has irregular number of spaces between words.
+    """
+    test_sentence = "Use this area for writing code or taking notes."
+    sentence_object = Sentence(test_sentence)
+    a_word = "Use"
+    expected = 0
+    actual = sentence_object.my_index(a_word)
+    assert actual == expected
+
+
+def test_my_index_a_word_not_in_sentence():
+    """
+    Test Sentence that has irregular number of spaces between words.
+    """
+    test_sentence = "Use this area for writing code or taking notes."
+    sentence_object = Sentence(test_sentence)
+    a_word = "U$e"
+    expected = None
+    actual = sentence_object.my_index(a_word)
+    assert actual == expected
