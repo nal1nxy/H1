d4e6e5053e7b87223a6e14caeb632699f9972756
NalinYet <nalin.yetukuri@unh.edu>
2024-09-25 18:00:44 -0400

    Test functions written in test_my_pop.py

--- test_my_pop.py
+++ test_my_pop.py
@@ -4,3 +4,28 @@ Initial developers: COMP 801 instructors
 Developer: Nalin Yetukuri
 Date: 9/24/2024
 """
+from sentence import Sentence
+
+
+def test_my_pop_legal_index():
+    """
+    Test Sentence that returns the word at the index given.
+    """
+    test_sentence = "Use this area for writing code or taking notes."
+    sentence_object = Sentence(test_sentence)
+    index = 4
+    expected_word = "writing"
+    actual_word = sentence_object.my_pop(index)
+    assert actual_word == expected_word
+
+
+def test_my_pop_illegal_index():
+    """
+    Test that the method returns None at the illegal index given.
+    """
+    test_sentence = "Use this area for writing code or taking notes."
+    sentence_object = Sentence(test_sentence)
+    index = 11
+    expected = None
+    actual = sentence_object.my_pop(index)
+    assert actual == expected
