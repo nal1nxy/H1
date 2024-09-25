5c3d1d07c65ecb781a9da7c7f8dc72cb5c09877d
NalinYet <nalin.yetukuri@unh.edu>
2024-09-25 16:31:33 -0400

    Test Functions written in test_my_join.py

--- test_my_join.py
+++ test_my_join.py
@@ -4,3 +4,28 @@ Initial developers: COMP 801 instructors
 Developer: Nalin Yetukuri
 Date: 9/24/2024
 """
+from sentence import Sentence
+
+
+def test_my_join_irregular_spaces():
+    """
+    Test Sentence that has irregular number of spaces between words.
+    """
+    test_sentence = "This       is a     string      in multiple   lines"
+    sentence_object = Sentence(test_sentence)
+    expected = "This is a string in multiple lines"
+    sentence_object.my_join()
+    actual = sentence_object.sentence
+    assert actual == expected
+
+
+def test_my_join_special_characters():
+    """
+    Test Sentence that has special characters as separate words.
+    """
+    test_sentence = "This !$ @ str!ng in mu|tip|e l!nes"
+    sentence_object = Sentence(test_sentence)
+    expected = "This is a string in multiple lines"
+    sentence_object.my_join()
+    actual = sentence_object.sentence
+    assert actual == expected
